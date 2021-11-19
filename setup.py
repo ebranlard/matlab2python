959aa25754530f473cbdc7800b4f822de8b3b287
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2021-11-18 17:55:17 -0700

    Switching to github actions for tests

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='matlab2python',
+    version='1.0',
+    description='matlab2python script from E. Branlard',
+    url='http://github.com/ebranlard/matlab2python/',
+    author='Emmanuel Branlard',
+    author_email='lastname@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False
+)
